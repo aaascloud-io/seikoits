2a824116fe627f39e21eaadf4acac99d7628f6d2
wang <xiangming.wang@i-focus.co.jp>
2021-02-17 15:24:46 +0900

    会社ソース提出（テスト）

--- server/src/test/java/com/ifocus/trackun/seikoits/SeikoitsApplicationTests.java
+++ server/src/test/java/com/ifocus/trackun/seikoits/SeikoitsApplicationTests.java
@@ -1,13 +1,130 @@
 package com.ifocus.trackun.seikoits;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ifocus.trackun.seikoits.entity.Seikoits_userEntity;
+import com.ifocus.trackun.seikoits.entity.Seikoits_userRepository;
+import com.ifocus.trackun.seikoits.model.Seikoits_companyModel;
+import com.ifocus.trackun.seikoits.service.CompanyService;
 
 @SpringBootTest
 class SeikoitsApplicationTests {
+
+	@Autowired
+	private Seikoits_userRepository seikoits_userRepository;
+
+	@Autowired
+	private CompanyService companyService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	/*
+	 * CompanyService
+	 * 一覧取得テストgetUnderCompanyInfos
+	 * 正常系
+	 *
+	 */
+	@Test
+	public void testGetUnderCompanyInfos() throws Exception {
+		// ifocus
+		Optional<Seikoits_userEntity> loginUserEntity = seikoits_userRepository.findById(1);
+
+		List<Seikoits_companyModel> returnList = companyService.getUnderCompanyInfos(loginUserEntity.get());
+
+		assertEquals(4, returnList.size());
+
+	}
+
+	/*
+	 * CompanyService
+	 * 会社詳細情報取得テストgetCompanyInfo
+	 * 正常系
+	 *
+	 */
+	@Test
+	public void testGetCompanyInfo() throws Exception {
+
+		Seikoits_companyModel model = companyService.getCompanyInfo(1);
+
+		assertEquals("2011501020673", model.getCorporatenumber());
+
+	}
+
+	/*
+	 * CompanyService
+	 * 会社更新テストupdateCompany
+	 * 正常系
+	 *
+	 */
+	@Test
+	public void testUpdateCompany() throws Exception {
+
+		// ifocus
+		Optional<Seikoits_userEntity> loginUserEntity = seikoits_userRepository.findById(1);
+
+		Seikoits_companyModel model = new Seikoits_companyModel();
+		model.setCompanyid(1);
+		model.setCorporatenumber("2011501029999");
+		model.setCompanyname("アイフォーカス株式会社");
+		model.setAddress("東京都中央区入船");
+		model.setIndustry("サービス業");
+		model.setMail("XXX@i-focus.co.jp");
+		model.setTel("03-1234-XXXX");
+		model.setFax("03-1234-YYYY");
+
+		Seikoits_companyModel updated = companyService.updateCompany(loginUserEntity.get(), model);
+
+		assertEquals("2011501029999", updated.getCorporatenumber());
+		assertEquals("アイフォーカス株式会社", updated.getCompanyname());
+		assertEquals("東京都中央区入船", updated.getAddress());
+		assertEquals("サービス業", updated.getIndustry());
+		assertEquals("XXX@i-focus.co.jp", updated.getMail());
+		assertEquals("03-1234-XXXX", updated.getTel());
+		assertEquals("03-1234-YYYY", updated.getFax());
+		assertEquals(0, updated.getLevel());
+
+	}
+
+	/*
+	 * CompanyService
+	 * 会社登録テストregisterCompany
+	 * 正常系
+	 *
+	 */
+	@Test
+	public void testRegisterCompany() throws Exception {
+
+		// ifocus
+		Optional<Seikoits_userEntity> loginUserEntity = seikoits_userRepository.findById(1);
+
+		Seikoits_companyModel model = new Seikoits_companyModel();
+		model.setCorporatenumber("201150102XXXX");
+		model.setCompanyname("アイフォーカス支社");
+		model.setAddress("大阪中央区");
+		model.setIndustry("サービス業");
+		model.setMail("YYY@i-focus.co.jp");
+		model.setTel("03-1234-ZZZZ");
+		model.setFax("03-1234-AAAA");
+
+		Seikoits_companyModel inserted = companyService.registerCompany(loginUserEntity.get(), model);
+
+		assertEquals("201150102XXXX", inserted.getCorporatenumber());
+		assertEquals("アイフォーカス支社", inserted.getCompanyname());
+		assertEquals("大阪中央区", inserted.getAddress());
+		assertEquals("サービス業", inserted.getIndustry());
+		assertEquals("YYY@i-focus.co.jp", inserted.getMail());
+		assertEquals("03-1234-ZZZZ", inserted.getTel());
+		assertEquals("03-1234-AAAA", inserted.getFax());
+		assertEquals(1, inserted.getLevel());
+
+	}
 }
