79e7b76a4f259979722fa58e55785bfb44d9d208
wang <xiangming.wang@i-focus.co.jp>
2021-02-17 18:18:40 +0900

    部門ソース提出

--- server/src/main/java/com/ifocus/trackun/seikoits/entity/Seikoits_divisionRepository.java
+++ server/src/main/java/com/ifocus/trackun/seikoits/entity/Seikoits_divisionRepository.java
@@ -1,8 +1,20 @@
 package com.ifocus.trackun.seikoits.entity;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 //@Repository
 public interface Seikoits_divisionRepository extends CrudRepository<Seikoits_divisionEntity, Integer> {
+
+	/*
+	 * 部門一覧情報取得
+	 *
+	 *
+	 */
+	@Query(value = "SELECT c.* FROM seikoits_division c WHERE c.companyid = :companyid ORDER BY divisionname",nativeQuery = true)
+	public List<Seikoits_divisionEntity> findCompanyDivisionList(@Param("companyid") Integer companyid);
 
 }
