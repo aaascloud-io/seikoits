9e43885431fffc66929e037ac834b3d7ed999278
唐 秘斯 <misi.tang@i-focus.co.jp>
2021-02-20 22:00:15 +0900

    所属木組織返却APIを追加

--- server/src/main/java/com/ifocus/trackun/seikoits/entity/Seikoits_groupRepository.java
+++ server/src/main/java/com/ifocus/trackun/seikoits/entity/Seikoits_groupRepository.java
@@ -1,8 +1,12 @@
 package com.ifocus.trackun.seikoits.entity;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 //@Repository
 public interface Seikoits_groupRepository extends CrudRepository<Seikoits_groupEntity, Integer> {
+
+	List<Seikoits_groupEntity> findByDivisionid(Integer divisionid);
 
 }
