b20e7e81615c3c72aafbb7eca1ac352dba2fff32
wang <xiangming.wang@i-focus.co.jp>
2021-02-22 09:54:37 +0900

    commit

--- server/src/main/java/com/ifocus/trackun/seikoits/constant/LevelConstant.java
+++ server/src/main/java/com/ifocus/trackun/seikoits/constant/LevelConstant.java
@@ -2,6 +2,13 @@ package com.ifocus.trackun.seikoits.constant;
 
 public class LevelConstant {
 
+	// i-focus
+	public static final Integer LEVEL_TOP = 0;
+
+	// 1級（正興）
+	public static final Integer LEVEL_ONE = 1;
+
+	// 2級
 	public static final Integer LEVEL_TWO = 2;
 
 }
